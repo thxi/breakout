5a7b26ed2a4cc25643850df60b08b2c14d637ef9
thxi <thxixl@protonmail.com>
2020-07-13 13:51:39 +0300

    drawing board

--- src/main.c
+++ src/main.c
@@ -2,50 +2,67 @@
 
 #include "raylib.h"
 
+typedef struct Brick {
+  Rectangle rect;
+  // int alive;
+  Color color;
+} Brick;
+
 int main(void) {
-  // Initialization
-  //--------------------------------------------------------------------------------------
   const int screenWidth = 600;
   const int screenHeight = 400;
 
-  InitWindow(screenWidth, screenHeight,
-             "raylib [core] example - keyboard input");
+  InitWindow(screenWidth, screenHeight, "Breakout");
 
-  Vector2 ballPosition = {(float)screenWidth / 2, (float)screenHeight / 2};
-  // float paddlePosition = {(float)screenWidth / 2, (float)screenHeight / 2};
+  Rectangle paddle = {
+      .x = screenWidth / 2, .y = 380, .width = 40, .height = 10};
 
-  SetTargetFPS(60);  // Set our game to run at 60 frames-per-second
-  //--------------------------------------------------------------------------------------
+  const int nLines = 6;
+  Color lineToColor[nLines] = {RED, ORANGE, GOLD, YELLOW, GREEN, BLUE};
+
+  float brickWidth = 20;
+  const int nBricks = screenWidth / brickWidth;
+  printf("nbricks %d\n", nBricks);
+  Brick bricks[nLines][nBricks];
+
+  for (int line = 0; line < nLines; line++) {
+    for (int i = 0; i < nBricks; i++) {
+      bricks[line][i] = (Brick){{.x = i * brickWidth,
+                                 .y = 50 + 10 * line,
+                                 .width = brickWidth,
+                                 .height = 10},
+                                .color = lineToColor[line]};
+    }
+  }
+  printf("end init\n");
+  SetTargetFPS(60);
 
   // Main game loop
   while (!WindowShouldClose())  // Detect window close button or ESC key
   {
-    // Update
-    //----------------------------------------------------------------------------------
-    if (IsKeyDown(KEY_RIGHT)) ballPosition.x += 2.0f;
-    if (IsKeyDown(KEY_LEFT)) ballPosition.x -= 2.0f;
-    if (IsKeyDown(KEY_UP)) ballPosition.y -= 2.0f;
-    if (IsKeyDown(KEY_DOWN)) ballPosition.y += 2.0f;
-    //----------------------------------------------------------------------------------
+    if (IsKeyDown(KEY_RIGHT)) paddle.x += 2.0f;
+    if (IsKeyDown(KEY_LEFT)) paddle.x -= 2.0f;
 
     // Draw
-    //----------------------------------------------------------------------------------
     BeginDrawing();
 
     ClearBackground(RAYWHITE);
 
-    DrawText("move the ball with arrow keys", 10, 10, 20, DARKGRAY);
+    DrawText("move the paddle with arrow keys", 10, 10, 20, DARKGRAY);
 
-    DrawCircleV(ballPosition, 50, MAROON);
+    DrawRectangleRec(paddle, RED);
+
+    for (int line = 0; line < nLines; line++) {
+      for (int i = 0; i < nBricks; i++) {
+        DrawRectangleRec(bricks[line][i].rect, bricks[line][i].color);
+      }
+    }
 
     EndDrawing();
-    //----------------------------------------------------------------------------------
   }
 
   // De-Initialization
-  //--------------------------------------------------------------------------------------
   CloseWindow();  // Close window and OpenGL context
-  //--------------------------------------------------------------------------------------
 
   return 0;
 }
